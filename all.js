fbfa3be2e96f99d00b4c02d112bd7aa3f0159b9d
MatthiasLiszt <mliszt.program@gmail.com>
2018-02-03 22:03:23 +0100

    provisoric

---
+++ all.js
@@ -0,0 +1,17 @@
+function loginpopup(){
+  document.getElementById('loginpopupbox').style.visibility="visible"; 
+  //alert('should show up');
+}
+
+function registerpopup(){
+  document.getElementById('registerpopupbox').style.visibility="visible"; 
+  
+}
+
+function loginSubmit(){
+  alert('login submitted');
+}
+
+function registerSubmit(){
+  alert('register submitted');
+}
